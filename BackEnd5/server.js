fda85dce3c0308d5e1038f934f362e4848fae1e9
LINOS NCHENA <linosnchena@gmail.com>
2020-11-02 17:07:31 +0100

    labeled as backend5

--- BackEnd5/server.js
+++ BackEnd5/server.js
@@ -1,7 +1,7 @@
-var express=require('express');
-var bodyparser=require('body-parser');
-var cors=require('cors');
-var rusanguLtD =require('./Controller/employee');
+var express = require('express');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var rusanguLtD = require('./Controller/employee');
 var http = require("http");
 
 //Use system configuration for port or use 1010 by default.
@@ -10,16 +10,19 @@ const app = express();
 const server = http.createServer(app);
 app.use(cors());
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false})); 
-app.use("/",rusanguLtD);
- 
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use("/", rusanguLtD);
+
 //if we are here then the specified request is not found
-app.use((req,res,next)=> {  const err = new Error("Not Found");  err.status = 404;  next(err);});
- 
+app.use((req, res, next) => {
+   const err = new Error("Not Found");
+   err.status = 404; next(err);
+});
+
 //all other requests are not implemented.
-app.use((err,req, res, next) => {
+app.use((err, req, res, next) => {
    res.status(err.status || 501);
-   res.json({ error: {code: err.status || 501,  message: err.message  } });
+   res.json({ error: { code: err.status || 501, message: err.message } });
 });
 
 server.listen(port)
