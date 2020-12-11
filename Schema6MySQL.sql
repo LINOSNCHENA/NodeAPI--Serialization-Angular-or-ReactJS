c225f592b56ca0a1e75607520808d4fa6c4f3f3d
Linos Nchena <linosnchena@gmail.com>
2020-12-11 06:29:19 +0100

    redecorations

--- Schema6MySQL.sql
+++ Schema6MySQL.sql
@@ -1,8 +1,8 @@
 use presly;
+
 DROP TABLE IF EXISTS bank1;
 CREATE TABLE bank1
-(
-    id SERIAL PRIMARY KEY,
+(   id SERIAL PRIMARY KEY,
     name1 VARCHAR(50) NOT NULL,
     dept VARCHAR(50) NOT NULL,
     post VARCHAR(50)NOT NULL,
@@ -10,8 +10,7 @@ CREATE TABLE bank1
     published boolean DEFAULT "1"
 );
 
-INSERT INTO bank1
-    ( name1, dept, post, salary )
+INSERT INTO bank1  ( name1, dept, post, salary )
 VALUES
     ("Mav1x", "Enginering", "Backend5", 21200 ),
     ( "Lorx", "MySQL-ReactJS", "Administrator", 18500 ),
@@ -19,5 +18,34 @@ VALUES
     ( "Wen", "MySQL-Angular", "CEO", 32500 ),
     ("Louis1x", "Enginering", "Backend5", 21200 );
 
-select*
-from bank1
+DROP TABLE IF EXISTS banker;
+CREATE TABLE banker
+(   id SERIAL PRIMARY KEY,
+    name1 VARCHAR(50) NOT NULL,
+    dept VARCHAR(50) NOT NULL,
+    post VARCHAR(50)NOT NULL,
+    salary int NOT NULL,
+    published boolean DEFAULT "1"
+);
+
+INSERT INTO banker ( name1, dept, post, salary )
+VALUES
+    ("Mav1x", "Enginering", "Backend5", 21200 ),
+    ( "Lorx", "MySQL-ReactJS", "Administrator", 18500 ),
+    ("Nikox", "Engineering", "Banker", 18500 ),
+    ( "Banker", "MySQL-Angular", "CEO", 32500 ),
+    ("Louis1x", "Enginering", "Backend5", 21200 );
+
+
+DROP TABLE IF EXISTS books;
+CREATE TABLE `books` (  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `books` ADD PRIMARY KEY (`id`);
+ALTER TABLE `books` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+select * from bank1, banker, books
